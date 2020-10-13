5c483321a41052696eb118773ea403d36fba75df
Miranda Ramirez <mxr17e@acu.edu>
2020-10-13 08:54:04 -0500

    Initial Commit

---
+++ DiceAppSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceAppSwift
+//
+//  Created by Miranda Ramirez Cospin on 10/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
