ba4adecb5b4d183407f57ab40327adb15998dba6
Miranda Ramirez Cospin <mxr17e@acu.edu>
2020-10-15 10:20:40 -0500

    Sounds and launch screen added

--- DiceAppSwift/ViewController.swift
+++ DiceAppSwift/ViewController.swift
@@ -6,15 +6,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView: UIImageView!
     @IBOutlet var criticalLabel: UILabel!
+    
+    var rollDiceSound: AVAudioPlayer?
+    var failSound: AVAudioPlayer?
+    var winSound: AVAudioPlayer?
+    var launchSound: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let launchPath = Bundle.main.path(forResource: "torpedo.mp3", ofType:nil)!
+        let launchurl = URL(fileURLWithPath: launchPath)
+        do {
+            launchSound = try AVAudioPlayer(contentsOf: launchurl)
+            launchSound?.play()
+        } catch {
+            // fail to load file
+        }
+        
+        let rollPath = Bundle.main.path(forResource: "rolldice.mp3", ofType:nil)!
+        let rollurl = URL(fileURLWithPath: rollPath)
+        do {
+            rollDiceSound = try AVAudioPlayer(contentsOf: rollurl)
+            rollDiceSound?.play()
+        } catch {
+            // fail to load file
+        }
+        
+        let winPath = Bundle.main.path(forResource: "zfanfare.mp3", ofType:nil)!
+        let winurl = URL(fileURLWithPath: winPath)
+        do {
+            winSound = try AVAudioPlayer(contentsOf: winurl)
+        } catch {
+            // fail to load file
+        }
+        
+        let failPath = Bundle.main.path(forResource: "failwah.mp3", ofType:nil)!
+        let failurl = URL(fileURLWithPath: failPath)
+        do {
+            failSound = try AVAudioPlayer(contentsOf: failurl)
+        } catch {
+            // fail to load file
+        }
+        
     }
     
     @IBAction func buttonGotPressed(){
@@ -23,7 +64,7 @@ class ViewController: UIViewController {
     
     func rollDice(){
         //dice stuff here
-        print("ROLL DICE!!!")
+        print("ROLLING DICE!!!")
         
         let rolledNumber = Int.random(in: 1...20)
         
@@ -31,10 +72,14 @@ class ViewController: UIViewController {
         
         diceImageView.image = UIImage(named: imageName)
         
+        rollDiceSound?.play()
+        
         if(imageName == "d1"){
             criticalLabel.text = "TRY AGAIN!!!"
+            failSound?.play()
         } else if (imageName == "d20"){
             criticalLabel.text = "CONGRATS!!!"
+            winSound?.play()
         }else{
             criticalLabel.text = ""
         }
