9846e2c7ff0de5c6b86f7d6fe79b6c39dc962d3c
Miranda Ramirez Cospin <mxr17e@acu.edu>
2020-10-15 09:27:32 -0500

    Class Activity: Dice images and view controller

--- DiceAppSwift/ViewController.swift
+++ DiceAppSwift/ViewController.swift
@@ -8,11 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageView: UIImageView!
+    @IBOutlet var criticalLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func buttonGotPressed(){
+        rollDice()
+    }
+    
+    func rollDice(){
+        //dice stuff here
+        print("ROLL DICE!!!")
+        
+        let rolledNumber = Int.random(in: 1...20)
+        
+        let imageName = "d\(rolledNumber)"
+        
+        diceImageView.image = UIImage(named: imageName)
+        
+        if(imageName == "d1"){
+            criticalLabel.text = "TRY AGAIN!!!"
+        } else if (imageName == "d20"){
+            criticalLabel.text = "CONGRATS!!!"
+        }else{
+            criticalLabel.text = ""
+        }
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollDice()
+    }
+    
+    
 
 
 }
